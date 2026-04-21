20f84b83db0d5a16856a4e1860292575aac917d2
Deepsa Baidya <workwithdeepsa@gmail.com>
2026-04-21 10:58:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Expense Tracker</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <h1>Expense Tracker</h1>
+
+  <input type="number" id="amount" placeholder="Amount">
+  <input type="text" id="category" placeholder="Category">
+  <button onclick="addExpense()">Add</button>
+
+  <ul id="list"></ul>
+
+  <script src="script.js"></script>
+</body>
+</html>
